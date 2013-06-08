bad093b5107b22c533831627d0409c729f700c77
Juan Carle <jcarle@gmail.com>
2013-06-08 18:10:13 -0300

    save settings

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,4 +1,4 @@
 	</div>
-	<div id="footer">footer!!</div>
+	<!-- <div id="footer"></div> -->
 </body>
 </html>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -98,3 +98,17 @@ function renderMenu($aMenu, $className = null){
 ?>
 <div class="content">
 	<h1><?php echo $title; ?></h1>
+	
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-41589815-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>	
