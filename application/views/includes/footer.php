08578087b1f9094f195a8157fc7e2dcccf061b6f
Juan Carle <jcarle@gmail.com>
2013-06-10 20:52:19 -0300

    login with FB

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -2,11 +2,27 @@
 	<!-- <div id="footer"></div> -->
 	
 <?php
+$scripts = '';
 if (isset($aServerData)) {
-echo '	
-<script>
-var SERVER_DATA = '.json_encode($aServerData).';
-</script>';
+	$scripts .= 'var SERVER_DATA = '.json_encode($aServerData).'; ';
+}
+
+if ($_SERVER['SERVER_NAME'] == 'www.jcarle.com.ar') {
+	$scripts .= "
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-41589815-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+";
+}
+if ($scripts != '') {
+	echo '<script type="text/javascript">'.$scripts.'</script>';		
 }
 ?>
 </body>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -16,6 +16,7 @@
 if (!isset($aJs)) {
 	$aJs = array();
 }
+
 // FIXME: pensar si esto se puede resolver de un modo mas elegante
 if ($view == 'includes/paginatedList') { 
 	$aJs[] = 'jquery.paginatedList-1.0.js';
@@ -99,16 +100,4 @@ function renderMenu($aMenu, $className = null){
 <div class="content">
 	<h1><?php echo $title; ?></h1>
 	
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-41589815-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>	
