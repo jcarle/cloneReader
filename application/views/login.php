95b3bbe7cf79353ad8e70748d0a96dc6fdc94890
Juan Carle <jcarle@gmail.com>
2013-06-14 22:28:46 -0300

    minify

--- application/views/login.php
+++ application/views/login.php
@@ -3,5 +3,5 @@ $this->load->view('includes/formValidation');
 ?>
 
 <a id='fb-login' href='#' onclick='facebookLogin()' >
-	<img src="<?php echo base_url()?>css/img/facebook-connect-button.png" border="0"/>
+	<img src="<?php echo base_url()?>assets/styles/img/facebook-connect-button.png" border="0"/>
 </a>
