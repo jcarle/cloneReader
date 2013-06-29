f70eedfd2bcde2f715086ab8dac0d5521263ef9d
Juan Carle <jcarle@gmail.com>
2013-06-29 12:09:26 -0300

    cache menu

--- application/config/config.php
+++ application/config/config.php
@@ -244,7 +244,7 @@ $config['encryption_key'] = 'puokvmbljgsieukjgsdlfkg8INwzUId3bg8INwzUId3bg';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'ci_session_clonereader';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
