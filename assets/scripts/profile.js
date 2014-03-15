a8e48ed6e39da01e369802ba04e34007f50ea025
Juan Carle <jcarle@gmail.com>
2014-03-14 22:05:41 -0300

    ajax app

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -1,24 +1,25 @@
 $.Profile = {
-	init: function() { 	
-		this.aTabs = $('.userProfile li')
+	init: function() {
+		this.$page	= $('.userProfile').parent().parent().parent();
+		this.aTabs 	= this.$page.find('.userProfile li');
 		for (var i=0; i<this.aTabs.length; i++) {
 			var tab = this.aTabs[i];
 
-			$(tab).find('a').click($.proxy(
+			this.$page.find(tab).find('a').click($.proxy(
 				function(event) {
 					var $link 		= $(event.target);
-					var $content 	= $($link.attr('href'));
+					var $content 	= this.$page.find($link.attr('href'));
 					this.loadTab($content.data('controller'), $content);
 				}
 			, this));
 		}
 		
-		$(this.aTabs[0]).find('a').click();
+		this.$page.find(this.aTabs[0]).find('a').click();
 	},
 	
 	loadTab: function(controller, $content) {
 		if ($content.children().length > 0) {
-			$('.content > .pageTitle h2').text( $content.find('.panel-heading').text() );
+			this.$page.find('.pageTitle h2').text( $content.find('.panel-heading').text() );
 			return;
 		}
 		
@@ -29,12 +30,12 @@ $.Profile = {
 				$.proxy(
 					function (result) {
 						if ($.hasAjaxErrorAndShowAlert(result) == true) { return; }
-						
+
 						result = $(result['result']);
 						$content.children().remove();
 						$content.html(result);
 						
-						$('.content > .pageTitle h2').text( $content.find('.panel-heading').text() );
+						this.$page.find('.pageTitle h2').text( $content.find('.panel-heading').text() );
 					}
 				, this),
 		});
