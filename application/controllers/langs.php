2b1b9bf61fa79fc9b596d9a9d5364e7914ef73f4
Juan Carle <jcarle@gmail.com>
2014-03-31 21:02:27 -0300

    ajax app

--- application/controllers/langs.php
+++ application/controllers/langs.php
@@ -9,7 +9,6 @@ class Langs extends CI_Controller {
 	
 	function change($langId) {
 		$this->load->model('Users_Model');
-				
 
 		$this->session->set_userdata('langId', $langId);
 		
@@ -19,7 +18,6 @@ class Langs extends CI_Controller {
 		}
 
 		$this->load->library('user_agent');
-		
 		// TODO: mejorar esto, para que redirija a un controler, no a una url completa; ej: http://jcarle.redirectme.net/dev/jcarle/cloneReader/login
 		if ($this->agent->is_referral()) {
 			redirect($this->agent->referrer());
