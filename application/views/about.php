800ca2392c2d178f3eafff4c76d12b2ac142a483
Juan Carle <jcarle@gmail.com>
2014-05-15 19:46:02 -0300

    about

--- application/views/about.php
+++ application/views/about.php
@@ -1,23 +1,27 @@
 <div class="">
-
-
-<p>
-	cloneReader is a clon of the old Google Reader.
-	<br/>	
-	Its open source and it is developed with codeigniter, simplepie, jquery, bootstrap.
-</p> 
-
-<h4>Features</h4>
-<ul>
-	<li>Export/Import OPML</li>
-	<li>Remote login </li>
-	<li>Responsive</li>
-	<li>Remote Storage</li> 
-	<li>Multi language. ['en', 'es', 'pt', 'zh']</li>
-	<li>Keyboard shortcut</li>	
-</ul>
-
-
-<a href="https://github.com/jcarle/cloneReader" class="btn btn-primary" data-skip-app-link="true" >
-	<i class="fa fa-github" ></i> Download on GitHub
-</a>
+	<p>
+		cloneReader is a clon of the old Google Reader.
+		<br/>	
+		Its open source and it is developed with 
+		<a data-skip-app-link="true" href="http://codeigniter.com">codeigniter</a>, 
+		<a data-skip-app-link="true" href="http://simplepie.org/">simplepie</a>, 
+		<a data-skip-app-link="true" href="http://jquery.com/">jquery</a>, 
+		<a data-skip-app-link="true" href="http://getbootstrap.com">bootstrap</a>,
+		<a data-skip-app-link="true" href="http://fortawesome.github.io/Font-Awesome/">font-awesome</a>.
+	</p> 
+	
+	<h4>Features</h4>
+	<ul>
+		<li>Export/Import OPML</li>
+		<li>Remote login </li>
+		<li>Responsive</li>
+		<li>Remote Storage</li> 
+		<li>Multi language. ['en', 'es', 'pt', 'zh']</li>
+		<li>Keyboard shortcut</li>	
+	</ul>
+	
+	
+	<a href="https://github.com/jcarle/cloneReader" class="btn btn-primary" data-skip-app-link="true" >
+		<i class="fa fa-github" ></i> Download on GitHub
+	</a>
+</div>
