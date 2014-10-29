a53dc3ca14b7c99bb2fd3ab85520a2fd35285e91
Juan Carle <jcarle@gmail.com>
2014-10-28 22:39:40 -0300

    page speed

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -20,24 +20,6 @@ echo '
 
 $this->carabiner->display('js');		
 
-if (!isset($langs)) {
-	$langs = array();
-}
-$langs      = getLangToJs($langs);
-$this->myjs->add(langJs($langs));
-$this->myjs->add(  '  $(\'.'.getPageName().'\').data(\'meta\', '.json_encode($meta).'); ');
-
-if (ENVIRONMENT == 'production' && config_item('google-analytics-Account') != '') {
-	$this->myjs->add( "
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', '".config_item('google-analytics-Account')."', 'auto');
-	ga('send', 'pageview');
-	" );
-}
-
 echo $this->myjs->getHtml();
 ?>		
 </body>
