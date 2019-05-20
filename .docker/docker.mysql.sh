82f15c834d3211758f2cb15d1789d28d67bb9ebb
Juan Carle <jcarle@edrans.com>
2019-05-20 16:23:09 +0200

    docker mysql

--- .docker/docker.mysql.sh
+++ .docker/docker.mysql.sh
@@ -3,10 +3,10 @@
 DIR=$(dirname "$(readlink -f "$0")")
 
 
-docker stop cloneReader-mysql;
-docker rm cloneReader-mysql;
+docker stop clonereader-mysql;
+docker rm clonereader-mysql;
 
-docker run --name cloneReader-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root \
+docker run --name clonereader-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root \
 	-e TZ=America/Buenos_Aires \
 	-v "$DIR/../.docker/dump:/tmp/backup" \
 	-v "$DIR/../.docker/var/lib/mysql:/var/lib/mysql" \
@@ -15,8 +15,8 @@ docker run --name cloneReader-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root \
 
 
 # to dump database:
-# docker exec  -u 0 -i -t  cloneReader-mysql bash
+# docker exec  -u 0 -i -t  clonereader-mysql bash
 # mysql -p
-# CREATE DATABASE cloneReader CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE DATABASE clonereader CHARACTER SET utf8 COLLATE utf8_general_ci;
 # CTR+C
-# mysql -p cloneReader < /tmp/backup/cloneReader.20190519.sql
+# mysql -p clonereader < /tmp/backup/clonereader.20190519.sql
